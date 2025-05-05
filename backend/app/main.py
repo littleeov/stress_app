b1ff13984ec36f23bd319a456c1493469cc289a3
littleeov <eov.grafd@yandex.ru>
2025-05-05 18:00:20 +0500

    new com

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from .models import Base
+from .database import engine
 
 app = FastAPI()
 
+# Инициализация БД
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Stress Detection API"}
